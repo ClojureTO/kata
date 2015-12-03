916fc419c1738e33ee1fe5fab6c4f6f67380f169
Xiyang Chen <settinghead@gmail.com>
2015-12-02 19:56:20 -0500

    revert to simple optimization for now

--- project.clj
+++ project.clj
@@ -69,7 +69,7 @@
                            {:app
                             {:source-paths ["env/prod/cljs"]
                              :compiler
-                                           {:optimizations :advanced
+                                           {:optimizations :simple
                                             :pretty-print false
                                             :closure-warnings
                                                            {:externs-validation :off :non-standard-jsdoc :off}}}}}
