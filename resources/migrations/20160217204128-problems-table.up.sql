43a4e5f6d6306943d0e2fada8f83122631e34b14
g026r <andrew.gammell@gmail.com>
2016-04-13 20:40:58 -0400

    Add some dummy data to the DB.

--- resources/migrations/20160217204128-problems-table.up.sql
+++ resources/migrations/20160217204128-problems-table.up.sql
@@ -8,3 +8,12 @@ CREATE TABLE problems
  description VARCHAR(500) NOT NULL,
  code VARCHAR(300) NOT NULL,
  solved BOOLEAN NOT NULL DEFAULT false);
+
+INSERT INTO problems (title, submitter, description, code)
+ values ('This is a test problem.', 'Alice', 'Test description', '(+ 1 1)');
+
+INSERT INTO problems (title, submitter, description, code)
+ values ('The 2nd Problem', 'Bob', 'Another description', '(* 2 2)');
+
+INSERT INTO problems (title, submitter, description, code)
+ values ('The 3rd and final problem', 'Charles', 'No description for you!', '(print "hello"');
