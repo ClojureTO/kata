81f6b5fa3836470e6fe25be19746be3bc1929416
Dmitri Sotnikov <dmitri.sotnikov@gmail.com>
2016-04-20 19:27:09 -0400

    merged submit-problem page

---
+++ env/dev/clj/kata/env.clj
@@ -0,0 +1,11 @@
+(ns kata.env
+  (:require [selmer.parser :as parser]
+            [clojure.tools.logging :as log]
+            [kata.dev-middleware :refer [wrap-dev]]))
+
+(def defaults
+  {:init
+   (fn []
+     (parser/cache-off!)
+     (log/info "\n-=[kata started successfully using the development profile]=-"))
+   :middleware wrap-dev})
