f98922308eabdd05417791b28bb9079ece5d7bb5
g026r <g026r@users.noreply.github.com>
2016-05-04 20:31:21 -0400

    Clear the editor on submit. (#47)
    Clear the editor on submit.
    
    * Clear the editor on submit.
    
    * Cleanup.

--- src/cljs/kata/pages/display_problems.cljs
+++ src/cljs/kata/pages/display_problems.cljs
@@ -26,13 +26,11 @@
 (defn apply-filters
   ""
   [filters table-contents]
-  (println "Filters " @filters)
-  (println "table-contents " table-contents)
-  (println "applying filters..."
-           (reduce
-             (fn [table filter]
-               (filter table))
-             table-contents @filters))
+  (reduce
+    (fn [table filter]
+      (filter table))
+    table-contents @filters)
+  ;)
   (reduce
     (fn [table filter]
       (filter table))
